348717037ccf0b5b28c30b0b28356fbdf67929b2
Lingyuan Ke <kelingyuan@gmail.com>
2016-07-11 01:23:41 +0100

    boilerplate

---
+++ client/main.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import 'styles/main.scss';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import Index from 'components/Index/Index';
+import Map from './Map';
+
+// render(<Index items={[1,2,3]} />, document.getElementById('js-main'));
