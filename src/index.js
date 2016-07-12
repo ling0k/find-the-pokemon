177240488c0853a0cc17047556b6e2408a613de8
Wei Wang <weizhi.station.wang@gmail.com>
2016-07-11 23:45:29 -0700

    add parse npm dependency

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Map from './Map';
 
+// import Parse from 'parse';
+// Parse.initialize(AppId, JsKey);
+// window.Parse = Parse;
+
 ReactDOM.render(<App />, document.getElementById('root'));
